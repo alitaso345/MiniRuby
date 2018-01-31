040917b4db525e4236c31293f3b41ae8bf8b7d14
alitaso345 <a.maruyama@uec.ac.jp>
2018-01-31 23:10:30 +0900

    Add sample tree

---
+++ sample_tree.rb
@@ -0,0 +1,18 @@
+def preorder(tree)
+  p tree[0]
+  if tree[0].start_with?("節")
+    preorder(tree[1])
+    preorder(tree[2])
+  end
+end
+
+leaf_a = ["葉A"]
+leaf_b = ["葉B"]
+leaf_c = ["葉C"]
+leaf_d = ["葉D"]
+
+node2 = ["節2", leaf_a, leaf_b]
+node3 = ["節3", leaf_c, leaf_d]
+node1 = ["節1", node2, node3]
+
+preorder(node1)
