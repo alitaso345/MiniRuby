edcfce298767cb87bb2d320cb0b9a3025c79a411
alitaso345 <a.maruyama@uec.ac.jp>
2018-02-01 23:37:32 +0900

    最大の葉を返す関数を追加

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,5 @@
 require 'minruby'
+
 def evaluate(tree)
   case tree[0]
   when 'lit'
@@ -42,10 +43,27 @@ def evaluate(tree)
   end
 end
 
+#tree->integer 木を受け取って一番大きい値の葉を返す
+def max(tree)
+  if tree[0] == 'lit'
+    tree[1]
+  else
+    left = max(tree[1])
+    right = max(tree[2])
+
+    if left > right
+      left
+    else
+      right
+    end
+  end
+end
 str = gets
 
 tree = minruby_parse(str)
 
 answer = evaluate(tree)
+max = max(tree)
 
 p answer
+p max
